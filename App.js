a16f404a3fd49d00d5bad8273ddbbd4922baab3c
Azeb.K <54040527+azebkidanemariam@users.noreply.github.com>
2021-05-28 02:08:17 +0200

    added dependencies,build basic components with text,view,image,stylesheet and safeareaview

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, SafeAreaView, Text, View, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  let Image_Http_URL = {
+    uri: 'https://reactnativecode.com/wp-content/uploads/2017/05/react_thumb_install.png',
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.heading}>
+          Here are some boxes of different colours
+        </Text>
+        <View style={[styles.box, styles.cyan]}>
+          <Text style={styles.text}>Cyan #2aa198</Text>
+        </View>
+        <View style={[styles.box, styles.blue]}>
+          <Text style={styles.text}>Blue #268bd2</Text>
+        </View>
+        <View style={[styles.box, styles.orange]}>
+          <Text style={styles.text}>Orange #cb4b16</Text>
+        </View>
+        <View style={[styles.box, styles.magneta]}>
+          <Text style={styles.text}>Magenta #d33682</Text>
+        </View>
+      <Image
+        source={Image_Http_URL}
+        style={{ height: 200, resizeMode: 'stretch' }}
+      />
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 10,
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  text: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  box: {
+    padding: 10,
+    borderRadius: 3,
     justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
+  },
+  magneta: {
+    backgroundColor: '#d33682',
   },
 });
+
+export default App;
