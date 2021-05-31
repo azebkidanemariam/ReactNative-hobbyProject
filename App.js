51654a89f19707ede5b17a8ee06ab9552b395764
Azeb.K <54040527+azebkidanemariam@users.noreply.github.com>
2021-05-31 11:10:06 +0200

    added color component to display colors dynamically

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Image, SafeAreaView, Text, View, StyleSheet } from 'react-native';
-
+import ColorBox from './components/ColorBox';
 const App = () => {
   let Image_Http_URL = {
     uri: 'https://reactnativecode.com/wp-content/uploads/2017/05/react_thumb_install.png',
@@ -11,22 +11,16 @@ const App = () => {
         <Text style={styles.heading}>
           Here are some boxes of different colours
         </Text>
-        <View style={[styles.box, styles.cyan]}>
-          <Text style={styles.text}>Cyan #2aa198</Text>
-        </View>
-        <View style={[styles.box, styles.blue]}>
-          <Text style={styles.text}>Blue #268bd2</Text>
-        </View>
-        <View style={[styles.box, styles.orange]}>
-          <Text style={styles.text}>Orange #cb4b16</Text>
-        </View>
-        <View style={[styles.box, styles.magneta]}>
-          <Text style={styles.text}>Magenta #d33682</Text>
-        </View>
-      <Image
-        source={Image_Http_URL}
-        style={{ height: 200, resizeMode: 'stretch' }}
-      />
+
+        <ColorBox colorName="Cyan" hexCode="#2aa198" />
+        <ColorBox colorName="Blue" hexCode="#268bd2" />
+        <ColorBox colorName="Orange" hexCode="#cb4b16" />
+        <ColorBox colorName="Magneta" hexCode="#d33682" />
+
+        <Image
+          source={Image_Http_URL}
+          style={{ height: 200, resizeMode: 'stretch' }}
+        />
       </View>
     </SafeAreaView>
   );
@@ -45,25 +39,6 @@ const styles = StyleSheet.create({
   text: {
     fontWeight: 'bold',
     color: 'white',
-  },
-  box: {
-    padding: 10,
-    borderRadius: 3,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  cyan: {
-    backgroundColor: '#2aa198',
-  },
-  blue: {
-    backgroundColor: '#268bd2',
-  },
-  orange: {
-    backgroundColor: '#cb4b16',
-  },
-  magneta: {
-    backgroundColor: '#d33682',
   },
 });
 
